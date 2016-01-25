a652bb05cce5afba713adb9d6c17961a3d48411e
Federico G. Schwindt <fgsch@lodoss.net>
2016-01-25 10:55:14 +0000

    Fail earlier if varnish.{m4,pc} are missing

--- libvmod-vsf/autogen.sh
+++ libvmod-vsf/autogen.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-autoreconf -vif
+dataroot=$(pkg-config --variable=datarootdir varnishapi 2>/dev/null)
+if [ -z "$dataroot" ] ; then
+	cat <<_EOF
+
+No package 'varnishapi' found
+
+Consider adjusting the PKG_CONFIG_PATH environment variable if you
+installed software in a non-standard prefix.
+
+_EOF
+	exit 1
+fi
+autoreconf -vif -I${dataroot}/aclocal
